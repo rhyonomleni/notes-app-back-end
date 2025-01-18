1c118f1de66081decd40d770d35a6ea368bc4125
rhyonomleni <93315228+rhyonomleni@users.noreply.github.com>
2025-01-18 12:32:01 +0700

    oke

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByHandler, editNoteByHandler, deleteNoteByIdHandler } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNoteByHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes{id}',
+        handler: editNoteByHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    }
+]
+
+
+module.exports = routes;
---
+++ src/server.js
@@ -0,0 +1,24 @@
+const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+    routes: {
+        cors: {
+          origin: ['*'],
+        },
+      },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+
+  //const response = h.response({ error: false, message: 'Catatan berhasil ditambahkan' });
+  //response.header('Access-Control-Allow-Origin', 'http://notesapp-v1.dicodingacademy.com');
+};
+
+init();
